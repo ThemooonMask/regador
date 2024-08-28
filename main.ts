80c2d9e6b8ccad2a5a95bc1a5aa2424ab2fa441e
ThemooonMask <165193094+ThemooonMask@users.noreply.github.com>
2024-08-28 14:01:33 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,33 @@
+let Umidade = 0
+basic.showIcon(IconNames.Target)
+datalogger.setColumnTitles("Unidade de Terra")
+robotbit.Servo(robotbit.Servos.S1, 50)
+basic.forever(function () {
+    Umidade = pins.digitalReadPin(DigitalPin.P1)
+    datalogger.log(datalogger.createCV("Unidade de Terra", pins.digitalReadPin(DigitalPin.P1)))
+    if (Umidade > 1000) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # # # # #
+            . . # . .
+            . . # . .
+            `)
+    } else if (Umidade > 800) {
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # # # .
+            . . # . .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
